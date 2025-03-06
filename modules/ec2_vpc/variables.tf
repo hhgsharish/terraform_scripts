b1ac8cff91e047aeb72e5e7cb6fc97230c716de4
Harisha G <hhgsharish@gmail.com>
2025-03-06 20:22:33 +0530

    Create variables.tf

---
+++ modules/ec2_vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2"
+  type        = string
+}
