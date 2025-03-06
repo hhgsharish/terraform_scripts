fa006adc2c86cbc7701fade8823b269e85bf70fc
Harisha G <hhgsharish@gmail.com>
2025-03-06 20:22:06 +0530

    Create main.tf

---
+++ modules/ec2_vpc/main.tf
@@ -0,0 +1,46 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "${terraform.workspace}-vpc"
+  }
+}
+
+resource "aws_security_group" "allow_ssh_http" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${terraform.workspace}-security-group"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
+  tags = {
+    Name = "${terraform.workspace}-instance"
+  }
+}
